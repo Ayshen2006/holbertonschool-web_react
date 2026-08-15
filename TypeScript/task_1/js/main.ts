31e1d1882c3242233bb88ca27aabad7e6e525c06
Ayshan <13561@holbertonstudents.com>
2026-08-15 13:02:04 -0700

    okk

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -43,3 +43,32 @@ const printTeacher: printTeacherFunction = (
 };
 
 console.log(printTeacher('John', 'Doe'));
+
+interface StudentClass {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClass;
+}
+
+class StudentClass implements StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
