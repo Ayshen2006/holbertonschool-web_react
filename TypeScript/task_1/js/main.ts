1f562bbd3269879cd3369a1f5356d5f43e15391d
Ayshan <13561@holbertonstudents.com>
2026-08-15 13:45:41 -0700

    4 is ok

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,61 +1,13 @@
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [property: string]: any;
-}
-
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
-};
-
-console.log(teacher3);
-
-interface Directors extends Teacher {
-  numberOfReports: number;
-}
-
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-
-console.log(director1);
-
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
-const printTeacher: printTeacherFunction = (
-  firstName: string,
-  lastName: string,
-): string => {
-  return `${firstName.charAt(0)}. ${lastName}`;
-};
-
-console.log(printTeacher('John', 'Doe'));
-
-interface StudentClass {
-  firstName: string;
-  lastName: string;
+interface StudentClassInterface {
   workOnHomework(): string;
   displayName(): string;
 }
 
-interface StudentConstructor {
-  new (firstName: string, lastName: string): StudentClass;
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
 }
 
-class StudentClass implements StudentClass {
+class StudentClass implements StudentClassInterface {
   firstName: string;
   lastName: string;
 
